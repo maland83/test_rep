dc12709f33333065ce18108ddef24fe199d3056f
Maland <maland83@gmail.com>
2022-03-28 12:06:26 +0300

    Final version with words count

--- viceversa/views.py
+++ viceversa/views.py
@@ -5,4 +5,10 @@ def home(request):
     return render(request, 'home.html')
 
 def reverse(request):
-    return render(request, 'reverse.html', {'userText': request.GET['usertext'][::-1]})
+
+    params = {'original_text': request.GET['usertext'],
+              'count_words': len(request.GET['usertext'].split()),
+              'reverse_text': request.GET['usertext'][::-1]
+              }
+
+    return render(request, 'reverse.html', params)
